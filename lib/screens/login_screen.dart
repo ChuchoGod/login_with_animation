7d349192574349c58e2ce71de61f4bc328c89100
ChuchoGod <emiliocamas13@gmail.com>
2025-09-04 10:15:36 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,16 +9,62 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _obscurePassword = true;
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       //Evita nudge o cámaras frontales en moviles
       body: SafeArea(
+          child: Padding(
+        //Eje
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            Expanded(child: RiveAnimation.asset('assets/animated_login_character.riv'))
+            SizedBox(
+                width: size.width,
+                height: 200,
+                child:
+                    RiveAnimation.asset('assets/animated_login_character.riv')),
+            //Espacio entre el oso y el texto email
+            const SizedBox(height: 10),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    //ESQUINAS RODEADAS
+                    borderRadius: BorderRadius.circular(12),
+                  )),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              obscureText: _obscurePassword,
+              decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    // ← nuevo
+                    icon: Icon(
+                      _obscurePassword
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    //ESQUINAS RODEADAS
+                    borderRadius: BorderRadius.circular(12),
+                  )),
+            ),
           ],
-        )),
+        ),
+      )),
     );
   }
 }
